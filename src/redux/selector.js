6e5a9847a676270e60590539af84d0669dbac4e9
TosiaDan <antonina.ivanets@gmail.com>
2023-01-31 08:10:26 +0100

    adds create selector

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,15 +1,30 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter;
 
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
-export const selectFilteredContacts = state => {
-  const fiterContacts = state.filter
-    ? state.contacts.items.filter(contact =>
-        contact.name.toLowerCase().includes(state.filter)
-      )
-    : state.contacts.items;
+// export const selectFilteredContacts = state => {
+//   const fiterContacts = state.filter
+//     ? state.contacts.items.filter(contact =>
+//         contact.name.toLowerCase().includes(state.filter)
+//       )
+//     : state.contacts.items;
 
-  return fiterContacts;
-};
+//   return fiterContacts;
+// };
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, queryFilter) => {
+    const fiterContacts = queryFilter
+      ? contacts.filter(contact =>
+          contact.name.toLowerCase().includes(queryFilter)
+        )
+      : contacts;
+
+    return fiterContacts;
+  }
+);
